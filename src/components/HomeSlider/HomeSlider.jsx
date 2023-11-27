b2bf7a616407ee77403ed3df212064d6ad6c3863
riazakhanda017ksn <riazakhanda@gmail.com>
2023-11-27 16:53:40 +0600

    commit

--- src/components/HomeSlider/HomeSlider.jsx
+++ src/components/HomeSlider/HomeSlider.jsx
@@ -6,17 +6,17 @@ import banner from '../../assets/images/banner/banner.png'
 const HomeSlider = () => {
     return (
         <div style={{background:"#E8F2F3"}}>
-        <div data-aos="zoom-in" data-aos-duration="1500">
+        <div data-aos="zoom-in" data-aos-duration="1200">
             <Navbar/>
 
             <div className='slider' >
             <div className="container">
             <div className="position_____________relative">
             <div className="text_position_change">
-            <h1 className='text-end' data-aos="zoom-in-left" data-aos-duration="1200"
+            <h1 className='text-end' data-aos="zoom-in-left" data-aos-duration="1400"
          >تقرير إدارة تجربة العميل
          </h1>
-            <h5 className='text-end' data-aos="zoom-in-left" data-aos-duration="1500"
+            <h5 className='text-end' data-aos="zoom-in-left" data-aos-duration="1600"
             > إدارة تجربة العميل
             </h5>
             </div>
