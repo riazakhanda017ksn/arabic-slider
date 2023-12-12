785ee2386a675277f93ea128e10710c65741d5d7
riazakhanda017ksn <riazakhanda@gmail.com>
2023-12-12 13:08:06 +0600

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,8 @@
 import React, { useEffect } from 'react';
 import AOS from 'aos';
-import 'aos/dist/aos.css'; // Import the AOS CSS
-import Slider from './components/Slider/Slider';
-import Content from './components/Content/Content';
-import SecondPartContent from './components/SecondPartContent/SecondPartContent';
-import Slide from './components/Slide/Slide';
-import ThirdSlide from './components/ThirdSlide/ThirdSlide';
-import OtherContentSlide from './components/OtherContentSlide/OtherContentSlide';
-import FourthSlide from './components/FourthSlide/FourthSlide';
-import SixthSlide from './components/SixthSlide/SixthSlide';
-import SevenSlide from './components/SevenSlide/SevenSlide';
-import EighthSlide from './components/EighthSlide/EighthSlide';
-import NinthSlide from './components/NinthSlide/NinthSlide';
-import TenthSlide from './components/TenthSlide/TenthSlide';
-import ElevenSlide from './components/ElevenSlide/ElevenSlide';
-import TwelveSlide from './components/TwelveSlide/TwelveSlide';
-import ThirteenSlide from './components/ThirteenSlide/ThirteenSlide';
-import FourteenSlide from './components/FourteenSlide/FourteenSlide';
-import FifteenSlide from './components/FifteenSlide/FifteenSlide';
-import SixteenSlide from './components/SixteenSlide/SixteenSlide';
-import LastSlide from './components/LastSlide/LastSlide';
-import FiveSlide from './components/FiveSlide/FiveSlide';
-import HomeSlider from './components/HomeSlider/HomeSlider';
+import 'aos/dist/aos.css'; 
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Home from './components/HOME/Home';
 
 const App = () => {
 
@@ -29,28 +10,14 @@ const App = () => {
     AOS.init();
   }, []);
   return (
-    <div className='hero'>
-      <HomeSlider/>
-      <Content/>
-      <SecondPartContent/>
-      <Slide/>
-      <ThirdSlide/>
-      <OtherContentSlide/>
-      <FourthSlide/>
-      <FiveSlide/>
-      <SixthSlide/>
-      <SevenSlide/>
-      <EighthSlide/>
-      <NinthSlide/>
-      <TenthSlide/>
-      <ElevenSlide/>
-      <TwelveSlide/>
-      <ThirteenSlide/>
-      <FourteenSlide/>
-      <FifteenSlide/>
-      <SixteenSlide/>
-      <LastSlide/>
-    </div>
+  <>
+        <BrowserRouter>
+        <Routes>
+          <Route exact path="/home" element={<Home/>} />
+        </Routes>
+        </BrowserRouter>
+
+  </>
   );
 };
 
