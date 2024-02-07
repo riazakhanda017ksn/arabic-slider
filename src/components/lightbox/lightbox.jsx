2a1be038c1c69fda5aaa42ad052382c3ae100fda
sakib chowdhury <sakibchowdhury@sakibs-MacBook-Air.local>
2024-02-07 15:36:22 +0600

    new

--- src/components/lightbox/lightbox.jsx
+++ src/components/lightbox/lightbox.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import PropTypes from "prop-types"; // Import PropTypes
 import { Modal } from "antd";
 import "./Lightbox.css"; // Import CSS file for styling
