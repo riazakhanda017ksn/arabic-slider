6b3be7e48f92da4c6ae69b9fb14f27429f48b83e
sakib chowdhury <sakibchowdhury@sakibs-MacBook-Air.local>
2024-02-08 02:55:33 +0600

    new

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -20,111 +20,67 @@ const Header = (handleToggleMinimize) => {
       id: 1,
       to: "/slide-1",
       text: "تقرير إدارة تجربة العميل",
-<<<<<<< HEAD
-      src: " /banner/1.jpg",
-=======
       src: banner1,
->>>>>>> 1c09c6f9eca25f349a2fa34d2e5982f127ef8bd8
     },
     {
       id: 2,
       to: "/slide-2",
       text: "`مؤشرات الاداء`",
-<<<<<<< HEAD
-      src: " /banner/2nd.jpg",
-=======
       src: banner2,
->>>>>>> 1c09c6f9eca25f349a2fa34d2e5982f127ef8bd8
     },
     {
       id: 3,
       to: "/slide-3",
       text: "صوت العميل",
-<<<<<<< HEAD
-      src: " /frame/3.jpg",
-=======
       src: banner3,
->>>>>>> 1c09c6f9eca25f349a2fa34d2e5982f127ef8bd8
     },
     {
       id: 4,
       to: "/slide-4",
       text: "إدارة المنتجات`",
-<<<<<<< HEAD
-      src: " /banner/online-marketing.jpg",
-=======
       src: banner4,
->>>>>>> 1c09c6f9eca25f349a2fa34d2e5982f127ef8bd8
     },
     {
       id: 5,
       to: "/slide-5",
       text: "`المهام التشغيلية`",
-<<<<<<< HEAD
-      src: " /banner/slide5.jpg",
-=======
       src: banner5,
->>>>>>> 1c09c6f9eca25f349a2fa34d2e5982f127ef8bd8
     },
     {
       id: 6,
       to: "/slide-6",
       text: "موقع البنك",
-<<<<<<< HEAD
-      src: " /banner/slide6.jpg",
-=======
       src: banner6,
->>>>>>> 1c09c6f9eca25f349a2fa34d2e5982f127ef8bd8
     },
     {
       id: 7,
       to: "/slide-7",
       text: "المتسوق الخفي",
-<<<<<<< HEAD
-      src: " /banner/slide7.jpg",
-=======
       src: banner7,
->>>>>>> 1c09c6f9eca25f349a2fa34d2e5982f127ef8bd8
     },
     {
       id: 8,
       to: "/slide-8",
       text: "الفروع",
-<<<<<<< HEAD
-      src: " /banner/slide8.jpg",
-=======
       src: banner8,
->>>>>>> 1c09c6f9eca25f349a2fa34d2e5982f127ef8bd8
     },
     {
       id: 9,
       to: "/slide-9",
       text: "التواصل",
-<<<<<<< HEAD
-      src: " /banner/banner9.jpg",
-=======
       src: banner9,
->>>>>>> 1c09c6f9eca25f349a2fa34d2e5982f127ef8bd8
     },
     {
       id: 10,
       to: "/slide-10",
       text: " المشاركة المجتمعية",
-<<<<<<< HEAD
-      src: " /banner/10.jpg",
-=======
       src: banner10,
->>>>>>> 1c09c6f9eca25f349a2fa34d2e5982f127ef8bd8
     },
     {
       id: 11,
       to: "/slide-11",
       text: "`برنامج ولاء العميل`",
-<<<<<<< HEAD
-      src: " /banner/last.jpg",
-=======
       src: banner11,
->>>>>>> 1c09c6f9eca25f349a2fa34d2e5982f127ef8bd8
     },
   ];
 
